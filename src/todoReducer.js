77673093dda3b3185f33126e7e827c63c8a6775f
Luis Ángel Herrera <luis_mh@outlook.es>
2023-08-09 06:06:33 -0600

    Primera pantalla Todos

---
+++ src/todoReducer.js
@@ -0,0 +1,14 @@
+
+
+export const todoReducer = (initialState = [], action) => {
+
+    switch (action.type) {
+        case 'ABC':
+            
+            return initialState;
+    
+        default:
+            return initialState;
+    }
+
+}
